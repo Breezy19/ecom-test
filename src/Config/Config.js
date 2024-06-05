8348bb3df1149f49ea5f3aa9c0d962d6f6262a89
“Breezy19” <freij.akram@gmail.com>
2024-06-05 23:14:11 +0100

    push to deploy it

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -4,16 +4,17 @@ import 'firebase/storage';
 import 'firebase/firestore';
 import 'firebase/auth'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "****REDACTED****",
-  authDomain: "ecommerce-website-85737.firebaseapp.com",
-  projectId: "ecommerce-website-85737",
-  storageBucket: "ecommerce-website-85737.appspot.com",
-  messagingSenderId: "893071101658",
-  appId: "1:893071101658:web:86210a42d39b20c52f9fe3",
-  measurementId: "G-QZ8QDPE6QH",
-  databaseURL: "https://ecommerce-website-85737.firebaseio.com"
-};
+    apiKey: "****REDACTED****",
+    authDomain: "ecommerce-website-85737.firebaseapp.com",
+    databaseURL: "https://ecommerce-website-85737-default-rtdb.firebaseio.com/",
+    projectId: "ecommerce-website-85737",
+    storageBucket: "ecommerce-website-85737.appspot.com",
+    messagingSenderId: "893071101658",
+    appId: "1:893071101658:web:86210a42d39b20c52f9fe3",
+    measurementId: "G-QZ8QDPE6QH"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
